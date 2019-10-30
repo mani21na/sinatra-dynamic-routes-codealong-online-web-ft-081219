68ea9d382454e9368ae60ba8d8790911db96e6db
Hanna Lee <mani21na@gmail.com>
2019-10-30 05:21:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,4 +22,5 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2'
     @num1 = params[:num1].to_i 
     @num2 = params[:num2].to_i
+    "The first param is #{@num1}, the second param is #{@num2}"
 end
